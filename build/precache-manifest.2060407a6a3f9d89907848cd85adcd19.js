2654f5108109d35ad396488efd4975ef928d4590
wayre <wayre1@gmail.com>
2020-08-18 09:35:28 -0300

    ajustes para deploy heroku

---
+++ build/precache-manifest.2060407a6a3f9d89907848cd85adcd19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb8831584357af6f04c3cfc60fe14ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3293cf17e6f0a0d65fb1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52b791c8f7601f649893",
+    "url": "/static/js/2.1665d1f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1665d1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3293cf17e6f0a0d65fb1",
+    "url": "/static/js/main.346a4243.chunk.js"
+  },
+  {
+    "revision": "9a86d725e08e646bac3e",
+    "url": "/static/js/runtime-main.ed304b27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
